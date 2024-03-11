a3bf2a7d3fdbba10824d3992ac5f0afcda0a8fa4
Alice <160416707+amkblue@users.noreply.github.com>
2024-03-11 08:25:02 -0400

    Update essentials.js

--- essentials/essentials.js
+++ essentials/essentials.js
@@ -1,12 +1,8 @@
-
 /* fixed nav bar active
    sticky nav bar from udemy course */
-const nav = document.querySelector('.nav-bar'); // declare variable called nav has the selected element with class .nav-bar
-window.addEventListener('scroll', changeNav); // when the window is scrolled, the event handler, changeNav function, will get executed
-
-// if the window is scrolled past the height of the nav bar + 170 pixels,
-// then the active class will be added to the nav bar
-// else the active class will be removed from the nav bar
+const nav = document.querySelector('.nav-bar');
+window.addEventListener('scroll', changeNav);
+      
 function changeNav() {
     if (window.scrollY > nav.offsetHeight + 170) {
         nav.classList.add('active');
@@ -15,41 +11,39 @@ function changeNav() {
         nav.classList.remove('active');
     }
 }
-
-
-
+        
 /* pink hearts effect on header 
    double click heart from udemy course */
 const header = document.getElementById('header');
-   
+         
 header.addEventListener('dblclick', (e) => {
     createHeart(e);
 });
-   
+         
 const createHeart = (e) => {
     const heart = document.createElement('i');
     heart.classList.add('fa-solid');
     heart.classList.add('fa-heart');
-   
+         
     const x = e.clientX;
     const y = e.clientY;
-   
+         
     const leftOffset = e.target.offsetLeft;
     const topOffset = e.target.offsetTop;
-   
+         
     const xInside = x - leftOffset;
     const yInside = y - topOffset;
-   
+         
     heart.style.top = `${yInside}px`;
     heart.style.left = `${xInside}px`;
-   
+         
     header.appendChild(heart);
 }
-   
-/* add to cart functionality */
+         
+/* add to cart */
 const addCartButton = document.querySelectorAll('.add-to-cart-button');
 const cartCount = document.getElementById('count');
-   
+         
 let num = 0;
 addCartButton.forEach((button) => {
     button.addEventListener('click', () => {
